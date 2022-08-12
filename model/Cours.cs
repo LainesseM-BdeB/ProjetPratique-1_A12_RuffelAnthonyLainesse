09747017fdb80436abc58df0dcc2b2b1500410bc
addisan <addisan.anthony@gmail.com>
2022-08-12 11:02:17 -0400

    ok

--- model/Cours.cs
+++ model/Cours.cs
@@ -14,19 +14,35 @@ public class Cours
     private typeSalle typeSalleCours;
 
 
-    public Cours()
-    {
-    }
 
-    public Cours(string sigleCours, string nomCours, string categorieCours, typeSalle typeSalleCours)
+    public Cours(string nomCours, string categorieCours, typeSalle typeSalleCours, Programme programme)
     {
-        this.sigleCours = sigleCours;
+        this.sigleCours = GenererSigleCours(nomCours, programme);
         this.nomCours = nomCours;
         this.categorieCours = categorieCours;
         this.typeSalleCours = typeSalleCours;
     }
 
-    
+    public string GenererSigleCours(String nomCours, Programme programme)
+    {
+        string sigle;
+        if (nomCours.Length < 3)
+        {
+            sigle = this.nomCours;
+            for (int i = 0; i < 3 - nomCours.Length; i++)
+            {
+                sigle += "X";
+            }
+        }
+        else
+        {
+            sigle = nomCours.Substring(0, 3);
+        }
+
+        string month = DateTime.Now.Month.ToString();
+        string pair = (programme.DureeProgrammeEnMois % 2) == 0 ? "P" : "I";
+        return sigle + "-" + month + "-" + pair;
+    }
 
     public string SigleCours
     {
