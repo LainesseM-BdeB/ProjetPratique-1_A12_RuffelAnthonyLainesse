7213d1dda6b64e6cef331fe1aa99fb24180c7dfa
LainesseM-BdeB <0684941@bdeb.qc.ca>
2022-08-12 08:23:50 -0400

    Programme.cs ADDED all attributes, constructors and getter/setter.

--- model/Programme.cs
+++ model/Programme.cs
@@ -2,5 +2,56 @@
 
 public class Programme
 {
-    
+    private int idProgramme;
+    private string titreProgramme;
+    private DateTime dateCreation;
+    private int nombreDeCredit;
+    private int dureeProgrammeEnMois;
+
+    private static int nombreTotalProgramme = 0;
+
+    public Programme()
+    {
+    }
+
+    public Programme(int idProgramme, string titreProgramme, DateTime dateCreation, int nombreDeCredit, int dureeProgrammeEnMois)
+    {
+        this.idProgramme = idProgramme;
+        this.titreProgramme = titreProgramme;
+        this.dateCreation = dateCreation;
+        this.nombreDeCredit = nombreDeCredit;
+        this.dureeProgrammeEnMois = dureeProgrammeEnMois;
+    }
+
+    public int IdProgramme
+    {
+        get => idProgramme;
+        set => idProgramme = value;
+    }
+
+    public string TitreProgramme
+    {
+        get => titreProgramme;
+        set => titreProgramme = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public DateTime DateCreation
+    {
+        get => dateCreation;
+        set => dateCreation = value;
+    }
+
+    public int NombreDeCredit
+    {
+        get => nombreDeCredit;
+        set => nombreDeCredit = value;
+    }
+
+    public int DureeProgrammeEnMois
+    {
+        get => dureeProgrammeEnMois;
+        set => dureeProgrammeEnMois = value;
+    }
+
+    public static int NombreTotalProgramme => nombreTotalProgramme;
 }
