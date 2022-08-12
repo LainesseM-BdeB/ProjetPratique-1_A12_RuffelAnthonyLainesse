564f6af05e127f9136ffa40ae37a13caeb8f72bc
addisan <addisan.anthony@gmail.com>
2022-08-12 08:45:46 -0400

    classe cours attributes + properties

--- Main.cs
+++ Main.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ProjetPratique_1_A12_RuffelAnthonyLainesse.model;
+
 Console.WriteLine("Hello, World!");
+Cours cour1 = new Cours("sjhjshs", "skjhdjhdd", "jhsjhdd", 0);
+cour1.afficherDetailCours();
--- model/Cours.cs
+++ model/Cours.cs
@@ -1,6 +1,65 @@
 ﻿namespace ProjetPratique_1_A12_RuffelAnthonyLainesse.model;
 
+public enum typeSalle
+{
+    LABORATOIRE,
+    AUDITOIRE,
+    AMPHITHEATRE
+}
 public class Cours
 {
-    
+    private string sigleCours;
+    private string nomCours;
+    private string categorieCours;
+    private typeSalle typeSalleCours;
+
+
+    public Cours()
+    {
+    }
+
+    public Cours(string sigleCours, string nomCours, string categorieCours, typeSalle typeSalleCours)
+    {
+        this.sigleCours = sigleCours;
+        this.nomCours = nomCours;
+        this.categorieCours = categorieCours;
+        this.typeSalleCours = typeSalleCours;
+    }
+
+    public genererSigle(string nomCours)
+    {
+
+    }
+
+    public string SigleCours
+    {
+        get => sigleCours;
+        set => sigleCours = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string NomCours
+    {
+        get => nomCours;
+        set => nomCours = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public string CategorieCours
+    {
+        get => categorieCours;
+        set => categorieCours = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public typeSalle TypeSalleCours
+    {
+        get => typeSalleCours;
+        set => typeSalleCours = value;
+    }
+
+    public void afficherDetailCours()
+    {
+        Console.WriteLine(this.SigleCours);
+        Console.WriteLine(this.NomCours);
+        Console.WriteLine(this.CategorieCours);
+        Console.WriteLine(this.TypeSalleCours);
+    }
 }
